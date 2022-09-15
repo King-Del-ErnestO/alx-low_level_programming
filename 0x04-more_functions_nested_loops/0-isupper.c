47c1bfddc4f3593fcf4f6e331ecbb2366e28648c
AKUBUEZE DEL ERNEST <delernestoa@gmail.com>
2022-09-16 00:20:49 +0100

    Is Upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,12 @@
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
+int main(){
+	return 0;
+}
+
 int _isupper(int c)
-{
-	int main(){
-		
+{	
 	return (c >= 'A' && c <= 'Z');
-	}
+	
 }
